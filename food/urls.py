e771df940f800c3bd5dc6bc541890a6cb93dab9f
kamilsmr <kamils6035@gmail.com>
2022-08-16 21:38:02 +0300

    detail_start

--- food/urls.py
+++ food/urls.py
@@ -2,7 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    #/food/
     path('',views.index, name='index'),
+    #/food/1
+    path('<int:item_id>/',views.detail, name='detail'),
     path('item/',views.item, name='item'),
 
 
